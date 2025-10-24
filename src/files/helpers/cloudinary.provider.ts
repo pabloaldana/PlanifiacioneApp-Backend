388e272ab268e8bc62f266e35c3438f1e0fd740d
Pablo Aldana <pabloaldana.cipo@gmail.com>
2025-10-24 10:38:16 -0300

    fix

--- src/files/helpers/cloudinary.provider.ts
+++ src/files/helpers/cloudinary.provider.ts
@@ -14,20 +14,3 @@ export const CloudinaryProvider: Provider = {
     return cloudinary;
   },
 };
-
-
-
-
-
-
-//!VERSION CORTAN SIN ESCALABILIDAD
-// import { v2 as cloudinary } from 'cloudinary';
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-//   secure: true,
-// });
-
-// export default cloudinary;
