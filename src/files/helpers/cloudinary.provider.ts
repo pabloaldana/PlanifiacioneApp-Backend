a1ddfa39fa99b3ed9c7e510a77f566bbe0f0ce2e
Pablo Aldana <pabloaldana.cipo@gmail.com>
2026-07-17 20:45:50 -0300

    fix cloudinary

--- src/files/helpers/cloudinary.provider.ts
+++ src/files/helpers/cloudinary.provider.ts
@@ -8,8 +8,8 @@ export const CloudinaryProvider: Provider = {
   useFactory: (configService: ConfigService) => {
     cloudinary.config({
       cloud_name: configService.get('CLOUDINARY_NAME'),
-      api_key: configService.get('CLOUDINARY_API_KEY'),
-      api_secret: configService.get('CLOUDINARY_API_SECRET'),
+      api_key: configService.get('CLOUDINARY_KEY'),
+      api_secret: configService.get('CLOUDINARY_SECRET'),
     });
     return cloudinary;
   },
